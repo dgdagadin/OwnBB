ca19cf1541135876ba4351e7d5a2a61acccba50e
Dagadin Dmitry <dgdagadin@gmail.ru>
2012-04-10 22:24:33 +0400

    #Add: File "PrivateMessages.php" was added

---
+++ www/Kernel/PrivateMessages.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = microtime ();
+//Проверка определяющей переменной
+if (!isset ($VALIDATION) || $VALIDATION <> '1') {
+	exit ('Hi, hacker!');
+}
+
+echo 'Service is in developing<br />';
+
+echo microtime () - $a;
+
+?>
